c538829fbc63486ad53d35a0bc3e6b71655c9bcc
bender <kudin.dima@gmail.com>
2026-05-03 18:04:10 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { getVisualEditScript } from "@/utils/visual-edit-script";
 import { Roboto } from "next/font/google";
 import { Poppins } from "next/font/google";
 import { Figtree } from "next/font/google";
+import { Libre_Baskerville } from "next/font/google";
 
 
 
@@ -25,8 +26,14 @@ export const metadata: Metadata = {
 
 
 
-const figtree = Figtree({
-  variable: "--font-figtree",
+
+const libreBaskerville = Libre_Baskerville({
+  variable: "--font-libre-baskerville",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -38,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${figtree.variable} antialiased`}>
+        <body className={`${libreBaskerville.variable} ${inter.variable} antialiased`}>
           <Tag />
           {children}
           <script
